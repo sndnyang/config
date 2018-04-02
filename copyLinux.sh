4a15baf9b3bdb618d1b7c79f6530add3c04ce436
Xiulong Yang <xyang22@student.gsu.edu>
2018-04-02 11:08:03 -0400

    linux configure

---
+++ copyLinux.sh
@@ -0,0 +1,12 @@
+
+# cp pelicanconf.py github.io
+
+cp $HOME/.vimrc vim/vimrc_linux
+
+# cp -rf E:/software/vim/vimfiles/ftdetect vim
+# cp -rf E:/software/vim/vimfiles/ftplugin vim
+cp $HOME/.bashrc bashrc_linux
+
+echo "OK? enter anything to exit"
+
+read
