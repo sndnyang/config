e591e412dc8cbdc0e7aa043fe7fa8372cf66c968
sndnyangd <sndnyangd@gmail.com>
2019-06-28 15:08:30 -0400

    more alias and vim row number

--- copyLinux.sh
+++ copyLinux.sh
@@ -6,7 +6,6 @@ cp $HOME/.vimrc vim/vimrc_linux
 # cp -rf E:/software/vim/vimfiles/ftdetect vim
 # cp -rf E:/software/vim/vimfiles/ftplugin vim
 cp $HOME/.bashrc bashrc_linux
-cp $HOME/bin/switchBranch.sh shell/
 cp $HOME/.screenrc _screenrc
 
 echo "OK? enter anything to exit"
